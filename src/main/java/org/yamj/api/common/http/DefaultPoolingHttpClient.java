b652837d46ea3a97513ffc0add463644188c3336
modmax <modmax@abwesend.de>
2013-05-31 10:57:00 +0200

    Added default implementation for pooling http client

---
+++ src/main/java/org/yamj/api/common/http/DefaultPoolingHttpClient.java
@@ -0,0 +1,80 @@
+package org.yamj.api.common.http;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.params.HttpParams;
+
+public class DefaultPoolingHttpClient extends AbstractPoolingHttpClient {
+
+    public DefaultPoolingHttpClient() {
+        this(null, null);
+    }
+
+    public DefaultPoolingHttpClient(ClientConnectionManager conman) {
+        this(conman, null);
+    }
+
+    public DefaultPoolingHttpClient(HttpParams params) {
+        this(null, params);
+    }
+
+    public DefaultPoolingHttpClient(ClientConnectionManager connectionManager, HttpParams httpParams) {
+        super(connectionManager, httpParams);
+    }
+
+    @Override
+    public String requestContent(URL url) throws IOException {
+        return requestContent(url, null);
+    }
+
+    @Override
+    public String requestContent(URL url, Charset charset) throws IOException {
+        URI uri;
+        try {
+            uri = url.toURI();
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException("Invalid url " + url, ex);
+        }
+
+        return requestContent(uri, charset);
+    }
+
+    @Override
+    public String requestContent(String uri) throws IOException {
+        return requestContent(uri, null);
+    }
+
+    @Override
+    public String requestContent(String uri, Charset charset) throws IOException {
+        HttpGet httpGet = new HttpGet(uri);
+        return requestContent(httpGet, charset);
+    }
+
+    @Override
+    public String requestContent(URI uri) throws IOException {
+        return this.requestContent(uri, null);
+    }
+
+    @Override
+    public String requestContent(URI uri, Charset charset) throws IOException {
+        HttpGet httpGet = new HttpGet(uri);
+        return requestContent(httpGet, charset);
+    }
+
+    @Override
+    public String requestContent(HttpGet httpGet) throws IOException {
+        return requestContent(httpGet, null);
+    }
+
+    @Override
+    public String requestContent(HttpGet httpGet, Charset charset) throws IOException {
+        HttpResponse response = execute(httpGet);
+        return readContent(response, charset);
+    }
+}
