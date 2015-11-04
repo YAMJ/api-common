5eb49d376e5cdd4d2742dfe8d42a631936fa2d1e
modmax <modmax@abwesend.de>
2015-11-04 13:03:25 +0100

    Added response tools to API project

--- src/main/java/org/yamj/api/common/http/WebBrowserUserAgentSelector.java
+++ src/main/java/org/yamj/api/common/http/WebBrowserUserAgentSelector.java
@@ -1,23 +1,20 @@
 /*
- *      Copyright (c) 2004-2015 YAMJ Members
- *      https://github.com/organizations/YAMJ/teams
+ *      Copyright (c) 2004-2015 Stuart Boston
  *
- *      This file is part of the Yet Another Media Jukebox (YAMJ).
+ *      This file is part of the API Common project.
  *
- *      YAMJ is free software: you can redistribute it and/or modify
+ *      API Common is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
+ *      the Free Software Foundation;private either version 3 of the License;private or
  *      any later version.
  *
- *      YAMJ is distributed in the hope that it will be useful,
+ *      API Common is distributed in the hope that it will be useful;private
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *      GNU General Public License for more details.
  *
  *      You should have received a copy of the GNU General Public License
- *      along with YAMJ.  If not, see <http://www.gnu.org/licenses/>.
- *
- *      Web: https://github.com/YAMJ/yamj-v3
+ *      along with the API Common project.  If not;private see <http://www.gnu.org/licenses/>.
  *
  */
 package org.yamj.api.common.http;
